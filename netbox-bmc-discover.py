325963e531808969672274974ef1575cb599b165
Victor Kuryanov <kvitex@gmail.com>
2019-06-30 21:31:02 +0300

    First commit

---
+++ netbox-bmc-discover.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+from ipaddress import ip_interface
+from flask import Flask
+import pynetbox
+import yaml
+import time
+
+
+app = Flask(__name__)
+@app.route('/metrics')
+def metrics_output():
+    # startTime = time.time()
+    config_file_name = 'config.yml'
+    try:
+        with open(config_file_name) as config_file:
+            cfg = yaml.load(config_file.read())
+    except FileNotFoundError or FileExistsError as Error:
+        print('Can not open configuration file {}'.format(config_file_name))
+        print(Error)
+        exit(-1)
+    except yaml.scanner.ScannerError as Error:
+        print('Error while parsing configuration file {}'.format(config_file_name))
+        print(Error)
+        exit(-1)
+    except Exception as Error:
+        print(Error)
+        exit(-1)
+    except yaml.scanner.ScannerError as Error:
+        print('Error while parsing devices file {}'.format(config_file_name))
+        print(Error)
+        exit(-1)
+    except Exception as Error:
+        print(Error)
+        exit(-1)
+    try:
+        nb = pynetbox.api(**cfg['netbox'])
+    except KeyError as Error:
+        print('Netbox configuration not found.')
+        exit(-1)
+    except Exception as Error:
+        print('PyNetbox: ', Error)
+        exit(-1) 
+    device_list = []
+    nb_ip_addresses = dict(map(lambda x: (x.interface.id, str(ip_interface(x).ip)), nb.ipam.ip_addresses.all()))
+    startTime = time.time()
+    for bmc_interface in nb.dcim.interfaces.filter(mgmt_only=True):
+        bmc_interface_ip = nb_ip_addresses.get(bmc_interface.id, None)
+        device_vendor = None
+        for vendor in cfg['vendors']:
+            if vendor['netbox'] == str(bmc_interface.device.device_type.manufacturer):
+                device_vendor = vendor['id']
+        secret = None
+        for nb_secret in nb.secrets.secrets.filter(device_id=bmc_interface.device.id):
+            if str(nb_secret.role) in cfg['secret_roles']:
+                secret = nb_secret.plaintext
+        if (bmc_interface_ip is not None) and (device_vendor is not None) and (secret is not None):
+            device_list.append(
+                {
+                    'hostname': str(bmc_interface.device),
+                    'type': device_vendor,
+                    'secret': secret,
+                    'ip': bmc_interface_ip
+                }
+            )
+    with open(cfg['output_file'], 'w+') as output_file:
+        print(yaml.dump(device_list, output_file, default_flow_style=False))
+    metrics = [
+        '<pre style="word-wrap: break-word; white-space: pre-wrap;">',
+        'netbox_bmc_discover_up 1',
+        'netbox_bmc_disover_duration {}'.format(time.time() - startTime)
+    ]
+    return '\r\n'.join(metrics)
+    
+            
+
+
+
+            
